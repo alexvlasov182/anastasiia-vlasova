10dd278594b9d7e3bc3e5e16ef38b1dc2ba17eb3
Oleksandr Vlasov <drumlife182@gmail.com>
2024-10-02 11:13:44 +0200

    Checked ReconHub section

--- src/domains/components/ReconHub/ReconHubTakeAways.tsx
+++ src/domains/components/ReconHub/ReconHubTakeAways.tsx
@@ -7,7 +7,7 @@ const ReconHubTakeAways: React.FC = () => {
       <div className={styles.takeAwaysContainer}>
         <h2 className={styles.takeAwaysTitle}>TAKEAWAYS</h2>
         <p className={styles.takeAwaysParagraph}>
-          <span className={styles.takeAawaysMainColor}>Design is a constant iteration of improving the experience for the end user.</span><br />
+          <span className={styles.takeAwaysMainColor}>Design is a constant iteration of improving the experience for the end user.</span><br />
           <b>User testing shouldn’t stop after development.</b> Continuously gather feedback to refine and enhance the product.<br />
           <b>Always seek out and listen to user feedback.</b> It provides critical insights that drive meaningful improvements and ensure the product meets real needs.
         </p>
