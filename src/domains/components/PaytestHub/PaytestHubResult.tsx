91733110e0dd8d13e78695d2ee7cafde54da86e2
Oleksandr Vlasov <drumlife182@gmail.com>
2024-10-02 14:10:01 +0200

    Changed styles in PaytestHub

--- src/domains/components/PaytestHub/PaytestHubResult.tsx
+++ src/domains/components/PaytestHub/PaytestHubResult.tsx
@@ -3,10 +3,10 @@ import styles from './PaytestHub.module.scss'
 
 const PaytestHubResult: React.FC = () => {
   return (
-    <section className={styles.screensSection}>
-      <div className={styles.screensContainer}>
-        <h2 className={styles.screensTitle}>RESULT</h2>
-        <p className={styles.screensParagraph}>
+    <section className={styles.paytestHubResultSection}>
+      <div className={styles.paytestHubResultContainer}>
+        <h2 className={styles.paytestHubResultTitle}>RESULT</h2>
+        <p className={styles.paytestHubResultParagraph}>
           Considering the answers we collected during the usability tests, we updated our screens and added scripts to the same page. Now users can hover on the test step and immediately see the corresponding script line.
         </p>
       </div>
--- src/domains/components/PaytestHub/PytestHubBusinessImpact.tsx
+++ src/domains/components/PaytestHub/PytestHubBusinessImpact.tsx
@@ -3,14 +3,14 @@ import styles from './PaytestHub.module.scss'
 
 const PaytestHubBusinessImpact: React.FC = () => {
   return (
-    <section className={styles.researchSection}>
-      <div className={styles.researchContainer}>
-        <h2 className={styles.researchTitle}>BUSINESS IMPACT</h2>
-        <p className={styles.researchParagraph}>
+    <section className={styles.buisnessImpactSection}>
+      <div className={styles.businessImpactContainer}>
+        <h2 className={styles.businessImpactTitle}>BUSINESS IMPACT</h2>
+        <p className={styles.businessImpactParagraph}>
           By creating a software tool to run tests remotely, we can increase the number of our clients. At the moment, we can only offer them robots. But with PaytestHub, users don't need to install third party tools, everything will be in one place.
         </p>
 
-        <p className={styles.researchParagraph}>
+        <p className={styles.businessImpactParagraph}>
           We can find more customers if we keep everything they need in one place and, most importantly, simplify the process so that more people can use our product.
         </p>
       </div>
