3019bd0dab0473fc46646bd7dfcaad09853a45bd
Oleksandr Vlasov <drumlife182@gmail.com>
2024-10-20 21:54:47 +0200

    Added a new section to the work page

--- src/domains/components/Home/WorkDescription.tsx
+++ src/domains/components/Home/WorkDescription.tsx
@@ -15,6 +15,43 @@ const WorkDescription: React.FC = () => {
         </b>
         —because life’s complicated enough without confusing interfaces.
       </p>
+
+      <div className={styles.dividerContainer}>
+        <div className={styles.sectionDivider}></div>
+      </div>
+
+      <section className={styles.servicesSection}>
+        <p className={styles.servicesText}>Domains I have experience with:</p>
+        <div className={styles.servicesContainer}>
+          <div className={styles.serviceItem}>
+            <h3 className={styles.servicesTitle}>CRMs</h3>
+            <p className={styles.servicesText}>
+              Streamlining complex workflows, making financial operations more
+              efficient.
+            </p>
+          </div>
+          <div className={styles.serviceItem}>
+            <h3 className={styles.servicesTitle}>B2B Solutions</h3>
+            <p className={styles.servicesText}>
+              Creating products that fit business needs and improve user
+              satisfaction.
+            </p>
+          </div>
+          <div className={styles.serviceItem}>
+            <h3 className={styles.servicesTitle}>SaaS</h3>
+            <p className={styles.servicesText}>
+              Improving usability for large-scale businesses with diverse needs.
+            </p>
+          </div>
+          <div className={styles.serviceItem}>
+            <h3 className={styles.servicesTitle}>Startups</h3>
+            <p className={styles.servicesText}>
+              Bringing ideas to life, where agile, user-driven design made a
+              real difference.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
